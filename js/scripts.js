771c62b25e1c8c6c30d42e2ca9d9497f3de88424
Tom Chege <emdeechege@gmail.com>
2018-07-20 11:34:24 +0300

    if a player rolls one or holds fxns

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //business logic
-var player1= "";
-var player2= "";
+var player1 = "";
+var player2 = "";
 
 var throwDice = function() {
   return Math.floor(6 * Math.random()) + 1;
@@ -8,9 +8,25 @@ var throwDice = function() {
 
 function Player(turn) {
   this.roll = 0;
-  this.tempScore= 0;
-  this.totalScore= 0;
+  this.tempScore = 0;
+  this.totalScore = 0;
   this.turn = turn;
   this.playerName;
 }
 //if player rolls one?
+Player.prototype.rollone = function() {
+  if (this.roll === 1) {
+    this.tempScore = 0;
+    //changes return
+    alert("Sorry" + this.payerName + ",you rolled 1,your turn is over!")}
+  else {
+    this.tempScore +=this.roll;
+  }
+}
+//If you click Hold
+Player.prototype.hold = function() {
+  this.totalScore += this.tempScore;
+  this.tempScore = 0;
+  //triggers change of turn.changeTurn()
+  alert(this.playerName + ", turn over, next player");
+}
